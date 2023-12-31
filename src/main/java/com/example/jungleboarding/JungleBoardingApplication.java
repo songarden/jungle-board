daf624e267b369d5deeadbb4b906374e539d9d6d
garden <gkdlqkql6@naver.com>
2023-12-31 15:18:04 +0900

    first commit

---
+++ src/main/java/com/example/jungleboarding/JungleBoardingApplication.java
@@ -0,0 +1,13 @@
+package com.example.jungleboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungleBoardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungleBoardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jungleboarding/JungleBoardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jungleboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JungleBoardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
