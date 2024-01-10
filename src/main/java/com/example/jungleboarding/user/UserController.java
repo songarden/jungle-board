fbaeea3dbfac538593f52a6a64e09dcb39ee0254
garden <gkdlqkql6@naver.com>
2024-01-10 15:23:16 +0900

    Chore : User Controller roles filter enable

--- src/main/java/com/example/jungleboarding/user/UserController.java
+++ src/main/java/com/example/jungleboarding/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-//@UserAuthorize
+@UserAuthorize
 public class UserController {
     
     @Autowired
